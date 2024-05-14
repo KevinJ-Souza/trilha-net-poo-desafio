22839547223d667eb831003c7696d5af8db9bb82
Kevin Souza <kevindesouza181818@gmail.com>
2024-05-14 14:18:23 -0300

    Desafio concluídogit add .

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string IMEI, int memoria) : base(numero, modelo, IMEI, memoria)
+        { }
+        
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+{
+        Console.WriteLine($"Instalando o {nomeApp} no Nokia");
+        Console.WriteLine($"{nomeApp} instalado no Nokia com sucesso!");}
+
+
+
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+ Nokia nokia = new Nokia( numero: "123456",  modelo: "ABCDEF",  IMEI: "35987654321",  memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+
+Console.WriteLine("\nIphone: ");
+ Iphone iphone = new Iphone(numero: "654321",  modelo: "FEDCBA",  IMEI: "35123456789",  memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
